181de1b12626e51452ff142b9b1f84af7273ec06
gufran21 <gufranamu2004@gmail.com>
2024-08-08 00:02:35 +0530

    package name changed

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 REPO_NAME = "youtube_scrapper"
-PKG_NAME= "ytd"
-AUTHOR_USER_NAME = "gufran21"
+PKG_NAME= "yt-downloader"
+AUTHOR_USER_NAME ="gufran21"
 AUTHOR_EMAIL = "gufranamu2004@gmail.com"
 
 setup(
