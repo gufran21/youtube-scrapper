3111eccda25b2138a8385023a86a2aac4b84dfde
gufran21 <gufranamu2004@gmail.com>
2024-08-08 00:11:55 +0530

    some changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.4"
 REPO_NAME = "youtube_scrapper"
-PKG_NAME= "yt-downloader"
+PKG_NAME= "scrape-youtube"
 AUTHOR_USER_NAME ="gufran21"
 AUTHOR_EMAIL = "gufranamu2004@gmail.com"
 
