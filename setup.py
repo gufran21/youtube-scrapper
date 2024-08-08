5e36f713386e2215c964311c453c056d9562725a
gufran21 <gufranamu2004@gmail.com>
2024-08-08 15:25:44 +0530

    some changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 REPO_NAME = "youtube_scrapper"
 PKG_NAME= "scrape_youtube"
 AUTHOR_USER_NAME ="gufran21"
